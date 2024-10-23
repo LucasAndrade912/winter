00bc4ebaa2005ea43df3ac42209d7d11b8353e06
Lucas Andrade <lucasantos.dev@gmail.com>
2024-10-23 15:59:39 -0230

    Criando classe de conexao com o banco de dados

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import br.edu.ifpb.webFramework.persistence.Connection;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Connection.connect("localhost", 5432, "quentinha", "postgres", "ifpb");
     }
 }
---
+++ src/br/edu/ifpb/webFramework/persistence/Connection.java
@@ -0,0 +1,21 @@
+package br.edu.ifpb.webFramework.persistence;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connection {
+    private static java.sql.Connection manager;
+
+    public static void connect(String ip, Integer port, String dbName, String user, String password) {
+        if (manager != null) return; // Verifica se a conexão já existe
+
+        String url = "jdbc:postgresql://" + ip + ":" + port + "/" + dbName;
+
+        try {
+            manager = DriverManager.getConnection(url, user, password);
+            System.out.println("Connecting to " + ip + ":" + port + " database: " + dbName);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
