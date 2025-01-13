5c80e7d1ca1c3994ca6a951e7015f8938360afc7
Lucas Andrade <lucasantos.dev@gmail.com>
2025-01-13 18:06:26 -0300

    Criacao de classe para extrarir dados de classes

---
+++ src/br/edu/ifpb/webFramework/utils/ClassDataExtract.java
@@ -0,0 +1,30 @@
+package br.edu.ifpb.webFramework.utils;
+
+import br.edu.ifpb.webFramework.persistence.Column;
+import br.edu.ifpb.webFramework.persistence.ColumnFactory;
+import br.edu.ifpb.webFramework.persistence.annotations.Entity;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ClassDataExtract {
+    private static final ColumnFactory columnFactory = new ColumnFactory();
+
+    public static DataExtracted extract(Class<?> clzz) {
+        String className = clzz.getSimpleName();
+        Boolean isEntity = false;
+        Field[] fields = clzz.getDeclaredFields();
+        String tableName = "";
+        List<Column> columns = Arrays.stream(fields).map(columnFactory::createColumn).collect(Collectors.toList());
+
+        if (clzz.isAnnotationPresent(Entity.class)) {
+            Entity entity = clzz.getAnnotation(Entity.class);
+            isEntity = true;
+            tableName = entity.name().isEmpty() ? className : entity.name();
+        }
+
+        return new DataExtracted(className, isEntity, fields, fields.length, tableName, columns, columns.size());
+    }
+}
